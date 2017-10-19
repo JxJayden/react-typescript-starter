20710a7ad302a5836232cfb2eb84a04af21256f4
jayden <jxdjayden@gmail.com>
2017-10-19 15:43:06 +0800

    chore(browser): remove useless code
    remove useless code

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
-// const App = () => {
-//   return (
-//     <div>
-//       <p>Hello world!</p>
-//     </div>
-//   )
-// }
 
 console.log('2')
 
