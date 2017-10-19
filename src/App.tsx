c1c7c8cd9d85e3a76517fbb9077e46351f86e449
jayden <jxdjayden@gmail.com>
2017-10-19 15:15:06 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <h2>Welcome to React-TypeScript</h2>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import App from './App'
+// const App = () => {
+//   return (
+//     <div>
+//       <p>Hello world!</p>
+//     </div>
+//   )
+// }
+
+console.log('2')
+
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
